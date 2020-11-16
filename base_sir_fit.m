3469c4e18fab3794cf7ecdb9aa9b2ce4d0f559d9
bdlong17 <71035960+bdlong17@users.noreply.github.com>
2020-11-16 17:33:16 -0600

    Add files via upload

--- base_sir_fit.m
+++ base_sir_fit.m
@@ -2,18 +2,26 @@
 % Here is an example that reads in infection and fatalities from STL City
 % and loads them into a new matrix covidstlcity_full
 % In addition to this, you have other matrices for the other two regions in question
-load('COVIDdata.mat')
 
-covidstlcity_full = double(table2array(COVID_STLcity(:,[5:6])));
-covidstlcnty_full = double(table2array(COVID_STLcnty(:,[5:6])));
 covidstlmetro_full = double(table2array(COVID_STLmetro(:,[5:6])));
+covidstlmetro_1 = double(table2array(COVID_STLmetro(1:118,[5:6])));
+covidstlmetro_2 = double(table2array(COVID_STLmetro(119:217,[5:6])));
+covidstlmetro_3 = double(table2array(COVID_STLmetro(218:240,[5:6])));
+
 covidkcmetro_full = double(table2array(COVID_KCmetro(:,[5:6])));
+covidkcmetro_1 = double(table2array(COVID_KCmetro(1:110,[5:6])));
+covidkcmetro_2 = double(table2array(COVID_KCmetro(111:240,[5:6])));
+
 covidremainmo_full = double(table2array(COVID_remainMO(:,[5:6])));
+covidremainmo_1 = double(table2array(COVID_remainMO(1:100,[5:6])));
+covidremainmo_2 = double(table2array(COVID_remainMO(101:180,[5:6])));
+covidremainmo_3 = double(table2array(COVID_remainMO(181:235,[5:6])));
 
-coviddata = covidstlcnty_full; % TO SPECIFY
+covidstlcity = double(table2array(COVID_STLcity(129:229,[5:6])));
+
+coviddata = covidstlmetro_full; % TO SPECIFY
 t = length(coviddata(:, 1)); % TO SPECIFY
-pop = STLcntyPop*100000; % TO SPECIFY
-
+pop = 100000*(STLmetroPop); % TO SPECIFY
 % The following line creates an 'anonymous' function that will return the cost (i.e., the model fitting error) given a set
 % of parameters.  There are some technical reasons for setting this up in this way.
 % Feel free to peruse the MATLAB help at
@@ -35,7 +43,7 @@ b = [];
 % Hint: For example, the sum of the initial conditions should be
 % constrained
 % If you don't want such a constraint, keep these matrices empty.
-Af = [1, 1, 1, 1, 1, 1, 1];
+Af = [0, 0, 0, 1, 1, 1, 1];
 bf = [1];
 
 %% set up upper and lower bound constraints
@@ -43,31 +51,25 @@ bf = [1];
 % lb < x < ub
 % here, the inequality is imposed element-wise
 % If you don't want such a constraint, keep these matrices empty.
-ub = []';
-lb = []';
+ub = [1, 1, 1, 1, 1, 1, 1]';
+lb = [0, 0, 0, 0, 0, 0, 0]';
 
 % Specify some initial parameters for the optimizer to start from
-x0 = [.05; 0.001; 0.5; 1; 0; 0; 0]; 
+x0 = [0.003, 0.004, 0.66, 1, 0, 0, 0]; 
 
 % This is the key line that tries to opimize your model parameters in order to
 % fit the data
 % note tath you 
-x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
+x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub)
 
-%plot(Y);
-%legend('S','I','R','D');
-%xlabel('Time')
+Y_fit = siroutput_full(x,t);
 
-Y_fit = siroutput_full(x,t)*pop;
-
-
-% Make some plots that illustrate your findings.
-casesModel = Y_fit(:, 2);
-deathsModel = Y_fit(:, 4);
+casesModel = (1 - Y_fit(:, 1)) * pop;
+deathsModel = Y_fit(:, 4) * pop;
 figure;
 hold on
-
+% Make some plots that illustrate your findings.
 plot(casesModel);
 plot(deathsModel);
 plot(coviddata);
-legend("Model Cases", "Model Deaths", "Cases", "Deaths", 'Location', 'northwest');
+legend("Model Cases", "Model Deaths", "Cases", "Deaths");
