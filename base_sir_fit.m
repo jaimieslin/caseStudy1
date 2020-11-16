5540a7bb72d9c5a22cf84958fe71544ce832c44b
jaimieslin <48027533+jaimieslin@users.noreply.github.com>
2020-11-16 16:56:56 -0600

    Add files via upload

--- base_sir_fit.m
+++ base_sir_fit.m
@@ -2,16 +2,18 @@
 % Here is an example that reads in infection and fatalities from STL City
 % and loads them into a new matrix covidstlcity_full
 % In addition to this, you have other matrices for the other two regions in question
+load('COVIDdata.mat')
 
-covidstlcity_full = double(table2array(COVID_STLcity(:,[5:6])))./(100000*STLcityPop);
-covidstlcnty_full = double(table2array(COVID_STLcnty(:,[5:6])))./(100000*STLcntyPop);
-covidstlmetro_full = double(table2array(COVID_STLmetro(:,[5:6])))./(100000*(STLcityPop + STLcntyPop));
-covidkcmetro_full = double(table2array(COVID_KCmetro(:,[5:6])))./(100000*KCmetroPop);
-covidremainmo_full = double(table2array(COVID_remainMO(:,[5:6])))./(100000*remainMOPop);
+covidstlcity_full = double(table2array(COVID_STLcity(:,[5:6])));
+covidstlcnty_full = double(table2array(COVID_STLcnty(:,[5:6])));
+covidstlmetro_full = double(table2array(COVID_STLmetro(:,[5:6])));
+covidkcmetro_full = double(table2array(COVID_KCmetro(:,[5:6])));
+covidremainmo_full = double(table2array(COVID_remainMO(:,[5:6])));
 
-coviddata = covidstlcity_full; % TO SPECIFY
+coviddata = covidstlmetro_full; % TO SPECIFY
 t = length(coviddata(:, 1)); % TO SPECIFY
-pop = 100000 * STLcityPop; % TO SPECIFY
+pop = 100000*STLmetroPop;
+
 % The following line creates an 'anonymous' function that will return the cost (i.e., the model fitting error) given a set
 % of parameters.  There are some technical reasons for setting this up in this way.
 % Feel free to peruse the MATLAB help at
@@ -19,46 +21,52 @@ pop = 100000 * STLcityPop; % TO SPECIFY
 % and see the sectiono on 'passing extra arguments'
 % Basically, 'sirafun' is being set as the function siroutput (which you
 % will be designing) but with t and coviddata specified.
-sirafun= @(x)siroutput(x,t,coviddata);
+sirafun= @(x)siroutput(x,t,coviddata, pop);
 
 %% set up rate and initial condition constraints
 % Set A and b to impose a parameter inequality constraint of the form A*x < b
 % Note that this is imposed element-wise
 % If you don't want such a constraint, keep these matrices empty.
-A = [0, 0, 0, 0, 0, 1, 1];
-b = [0];
+A = [1, 1, 1, 1, 1, 1, 1];
+b = [1];
 
 %% set up some fixed constraints
 % Set Af and bf to impose a parameter constraint of the form Af*x = bf
 % Hint: For example, the sum of the initial conditions should be
 % constrained
 % If you don't want such a constraint, keep these matrices empty.
-Af = [0, 0, 0, 1, 1, 1, 1];
-bf = [1];
+Af = [];
+bf = [];
 
 %% set up upper and lower bound constraints
 % Set upper and lower bounds on the parameters
 % lb < x < ub
 % here, the inequality is imposed element-wise
 % If you don't want such a constraint, keep these matrices empty.
-ub = [1, 1, 1, 1, 1, 0, 0]';
-lb = [0, 0, 0, 0, 0, 0, 0]';
+ub = []';
+lb = []';
 
 % Specify some initial parameters for the optimizer to start from
-x0 = [0.003, 0.004, 0.66, 1, 0, 0, 0]; 
+x0 = [.05; 0.001; 0.5; 1; 0; 0; 0]; 
 
 % This is the key line that tries to opimize your model parameters in order to
 % fit the data
 % note tath you 
-x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub)
+x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
+
+%plot(Y);
+%legend('S','I','R','D');
+%xlabel('Time')
 
 Y_fit = siroutput_full(x,t);
 
-casesModel = 1 - Y_fit(:, 1);
-deathsModel = Y_fit(:, 4);
+
+% Make some plots that illustrate your findings.
+casesModel = Y_fit(:, 2)*pop;
+deathsModel = Y_fit(:, 4)*pop;
 figure;
 hold on
-% Make some plots that illustrate your findings.
+
 plot(casesModel);
 plot(deathsModel);
 plot(coviddata);
