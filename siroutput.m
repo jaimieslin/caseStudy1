26b7b381e382c738a79c3d5c65f34e1b7e154021
jaimieslin <48027533+jaimieslin@users.noreply.github.com>
2020-11-18 18:37:27 -0600

    Add files via upload

--- siroutput.m
+++ siroutput.m
@@ -3,7 +3,7 @@
 % t - the number of time-steps you wish to simulate
 % data - actual data that you are attempting to fit
 
-function f = siroutput(x,t,data, pop)
+function f = siroutput(x,t,data)
 
 % set up transmission constants
 k_infections = x(1);
@@ -26,19 +26,17 @@ B = zeros(4,1);
 % Set up the vector of initial conditions
 x0 = [ic_susc ic_inf, ic_rec, ic_fatality].';
 
-% simulate the SIRD model for t time-steps
+% Simulate the SIRD model for t time-steps
 sys_sir_base = ss(A,B,eye(4),zeros(4,1),1);
-y = lsim(sys_sir_base,zeros(t,1),linspace(0,t-1,t),x0)*pop;
+y = lsim(sys_sir_base,zeros(t,1),linspace(0,t-1,t),x0);
 
-% return a "cost".  This is the quantitity that you want your model to
-% minimize.  Basically, this should encapsulate the difference between your
-% modeled data and the true data. Norms and distances will be useful here.
-% Hint: This is a central part of this case study!  choices here will have
-% a big impact!
-casesModel = (y(:, 2));
+% Cases and deaths
+casesModel = (1-y(:, 1));
 casesActual = data(1:t, 1);
 deathsModel = y(:, 4);
 deathsActual = data(1:t, 2);
+
+% Return a "cost"
 f = (norm(casesModel - casesActual)) + (norm(deathsModel - deathsActual));
 
 end
